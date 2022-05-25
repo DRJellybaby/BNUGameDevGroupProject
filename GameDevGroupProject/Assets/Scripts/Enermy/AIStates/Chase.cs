9cc06706a723ed94a28e241dadf52d12ce7ddedb
Will Deeley <72080003+DRJellybaby@users.noreply.github.com>
2022-05-25 15:26:48 +0100

    models and FSM updated
    added testing scene
    added several models and annimations

--- GameDevGroupProject/Assets/Scripts/Enermy/AIStates/Chase.cs
+++ GameDevGroupProject/Assets/Scripts/Enermy/AIStates/Chase.cs
@@ -6,17 +6,31 @@ public class Chase<T> : AIState<T>
 {
     public Chase(T stateName, StateDrivenBrain controller, float minDuration) : base(stateName, controller, minDuration) { }
 
-    public override void Act()
-    {
-    }
+    private Transform lastSeenLocation;
 
     public override void OnEnter()
     {
         base.OnEnter();
+        brain.chasing = true;
+        lastSeenLocation = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        brain.chasePoint.GetComponent<Transform>().position = new Vector3(lastSeenLocation.position.x, lastSeenLocation.position.y, lastSeenLocation.position.z);
+        TurnToFace();
+        //brain.navMeshAgent.SetDestination(brain.chasePoint.GetComponent<Transform>().position);
     }
 
     public override void OnLeave()
     {
         base.OnLeave();
+    }
+
+    private void TurnToFace()
+    {
+        // Initially calculate rotation as a vector so that x and z components can be set to zero
+        Vector3 lookRotation = Quaternion.LookRotation(lastSeenLocation.gameObject.transform.position - brain.gameObject.transform.position).eulerAngles;
+        lookRotation.x = 0;
+        lookRotation.z = 0;
+        // All rotations should be applied as Quaternion so convert back
+        lookAtRotation = Quaternion.Euler(lookRotation);
+
     }
 }
--- GameDevGroupProject/Assets/Scripts/Enermy/StateDrivenBrain.cs
+++ GameDevGroupProject/Assets/Scripts/Enermy/StateDrivenBrain.cs
@@ -13,6 +13,9 @@ public class StateDrivenBrain : EnermyControler
     public bool wait;
     public float waitTime = 3;
 
+    public bool chasing;
+    public GameObject chasePoint;
+
     public enum AIStates { Idle, Chase, Attack, Hold, SearchArea, Return, Death };
     public FSM<AIStates> stateMachine;
     protected float thinkInterval = 0.4f;
@@ -89,7 +92,7 @@ public class StateDrivenBrain : EnermyControler
     public bool GuardAttackToDeath(State<AIStates> currentState) { return (healthLevel <= 0); }
     public bool GuardSearchAreaToChase(State<AIStates> currentState) { return (sight.CanSeeTarget()); }
     public bool GuardSearchAreaToHold(State<AIStates> currentState) { return ((sight.CanSeeTarget()) && (minimumStamina >= staminaLevel)); }
-    public bool GuardSearchToReturn(State<AIStates> currentState) { return (!senses.CanSeeTarget() && !wait); }//add wait timer
+    public bool GuardSearchAreaToReturn(State<AIStates> currentState) { return (!sight.CanSeeTarget() && !wait); }//add wait timer
     public bool GuardSearchAreaToDeath(State<AIStates> currentState) { return (healthLevel <= 0); }
     public bool GuardHoldToSearchArea(State<AIStates> currentState) { return (!sight.CanSeeTarget()); }
     public bool GuardHoldToChase(State<AIStates> currentState) { return (minimumStamina >= staminaLevel); }
