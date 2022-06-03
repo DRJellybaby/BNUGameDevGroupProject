d2ef4f157bee314119733fa0c1b73c74fa3a6452
vytautasKravcenka <72344410+vytautasKravcenka@users.noreply.github.com>
2022-06-03 16:23:42 +0100

    Super broken, but it works?

--- GameDevGroupProject/Assets/Scripts/items/weapon.cs
+++ GameDevGroupProject/Assets/Scripts/items/weapon.cs
@@ -18,15 +18,14 @@ public class weapon : MonoBehaviour
     //basic damage script for weapons, idea is they move with the animation, and so the colliders apply damage.
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("tags dont work");
         if (other != null)
         {
-            
             if (other.tag == "Enermy")
             {
                 enermyControler = other.GetComponent<EnermyControler>();
                 Debug.Log("hit an enermy");
                 enermyControler.takeDamage(playerStats.TotalDamage(this.gameObject));
+                Debug.Log(playerStats.TotalDamage(this.gameObject));
             }
             if (other.tag == "Player")
             {
